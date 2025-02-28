06c1b8fcce442b21de7431de96778c8bf2c41a60
Mateus da Rocha Borges <mateusborges99207@gmail.com>
2025-02-28 20:21:54 -0300

    aula 28-02

---
+++ Lista 1/src/exe2/Cliente.java
@@ -0,0 +1,12 @@
+package exe2;
+
+public class Cliente {
+    public int nroAgencia, nroConta;
+    public String nome;
+    public float saldo;
+
+    //método construtor sem parámetro
+    public Cliente(){
+        this.nome = "sem nome";
+    }
+}
